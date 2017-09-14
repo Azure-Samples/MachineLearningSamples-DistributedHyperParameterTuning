ffc339d6f570dc1090e6b358b28fbab1a513d254
Dmitry Pechyoni <pechyony@outlook.com>
2017-09-14 10:23:47 -0400

    fix directories

---
+++ Code/singleVMsmall.py
@@ -0,0 +1,22 @@
+import feature_engineering as fe
+from load_data import load_data
+
+if __name__ == "__main__":
+
+    load_data()
+
+    #################################################################
+    # Feature engineering
+    #################################################################  
+
+    # Create one-hot encoding of brand and model
+    train_brand, test_brand, train_model, test_model = fe.one_hot_brand_model(brand_model, gender_age_train, gender_age_test)
+
+    # Create weekday and hour features (represented using one-hot encoding)
+    train_weekday, train_hour, train_weekday_hour, test_weekday, test_hour, test_weekday_hour = fe.weekday_hour_features(events, gender_age_train, gender_age_test)
+
+
+
+                                                                                
+
+
